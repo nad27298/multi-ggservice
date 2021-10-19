27656efd3c67f75225e7039898c56d6b11e897f1
daona <daona@miagi-so.net>
2021-10-19 16:18:13 +0700

    add check configuration

--- MultiGoogleService/MultiGoogleService/Controllers/ViewController.swift
+++ MultiGoogleService/MultiGoogleService/Controllers/ViewController.swift
@@ -9,17 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    enum Configuration: String {
+
+        // MARK: - Configurations
+
+        case staging
+        case debug
+        case release
+
+        // MARK: - Current Configuration
+
+        static let current: Configuration = {
+            guard let rawValue = Bundle.main.infoDictionary?["Configuration"] as? String else {
+                fatalError("No Configuration Found")
+            }
+
+            guard let configuration = Configuration(rawValue: rawValue.lowercased()) else {
+                fatalError("Invalid Configuration")
+            }
+
+            return configuration
+        }()
+
+    }
+    
+    @IBOutlet weak var configLabel: UILabel!
+    
     var appIdOnAppStore: String? {
         return Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
         
         if let appId = appIdOnAppStore {
             print(appId)
         }
+        
+        print(Configuration.current)
+        
+        switch Configuration.current{
+        case Configuration.debug:
+            self.configLabel.text = "DEBUG"
+        case Configuration.staging:
+            self.configLabel.text = "STAGING"
+        case Configuration.release:
+            self.configLabel.text = "RELEASE"
+        }
+        
+//        #if DEBUG
+//        self.configLabel.text = "DEBUG"
+//        #elseif STAGING
+//        self.configLabel.text = "STAGING"
+//        #else
+//        self.configLabel.text = "RELEASE"
+//        #endif
         
     }
 
